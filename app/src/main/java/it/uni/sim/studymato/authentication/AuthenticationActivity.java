e7ac2adc9f9edf64abfe0c1d79998df303eadf99
Alnu73 <axel8744@gmail.com>
2022-10-26 16:14:31 +0200

    Refined AuthenticationActivity

--- app/src/main/java/it/uni/sim/studymato/authentication/AuthenticationActivity.java
+++ app/src/main/java/it/uni/sim/studymato/authentication/AuthenticationActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import it.uni.sim.studymato.R;
+import it.uni.sim.studymato.databinding.ActivityAuthenticationBinding;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    ActivityAuthenticationBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authentication);
+        binding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
