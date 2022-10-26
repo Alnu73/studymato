a6ccf16ab26cca740d956b0b70638f49ee3744bf
Alnu73 <axel8744@gmail.com>
2022-10-26 16:10:59 +0200

    AuthenticationActivity added

---
+++ app/src/main/java/it/uni/sim/studymato/authentication/AuthenticationActivity.java
@@ -0,0 +1,16 @@
+package it.uni.sim.studymato.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.uni.sim.studymato.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
