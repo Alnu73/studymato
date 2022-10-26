f18a654fe0d2f56e338f0a0746d269ac4f4ce666
Alnu73 <axel8744@gmail.com>
2022-10-26 17:58:14 +0200

    Authentication logic implemented in MainActivity

--- app/src/main/java/it/uni/sim/studymato/MainActivity.java
+++ app/src/main/java/it/uni/sim/studymato/MainActivity.java
@@ -2,13 +2,42 @@ package it.uni.sim.studymato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import it.uni.sim.studymato.authentication.AuthenticationActivity;
+import it.uni.sim.studymato.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null) {
+            Intent intent = new Intent(MainActivity.this, AuthenticationActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
