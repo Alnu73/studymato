9e3b15cda99f42c3d274fb1a54b1babae0342f0c
Alnu73 <axel8744@gmail.com>
2022-10-26 17:15:57 +0200

    Sign in logic implemented

--- app/src/main/java/it/uni/sim/studymato/authentication/SignInFragment.java
+++ app/src/main/java/it/uni/sim/studymato/authentication/SignInFragment.java
@@ -1,18 +1,27 @@
 package it.uni.sim.studymato.authentication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import it.uni.sim.studymato.MainActivity;
 import it.uni.sim.studymato.R;
 import it.uni.sim.studymato.databinding.FragmentSignInBinding;
 
@@ -43,6 +52,13 @@ public class SignInFragment extends Fragment {
                 (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.auth_nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
 
+        binding.btnlogin.setOnClickListener(v -> {
+            String email, password;
+            email = binding.email.getText().toString();
+            password = binding.passwd.getText().toString();
+            loginAccount(email, password);
+        });
+
         binding.tvRedirectLogin.setOnClickListener(v -> {
             NavDirections action = SignInFragmentDirections.actionSignInFragmentToSignUpFragment();
             navController.navigate(action);
@@ -55,5 +71,31 @@ public class SignInFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    private void loginAccount(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(requireActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Intent intent = new Intent(requireActivity(), MainActivity.class);
+        startActivity(intent);
+
     }
 }
