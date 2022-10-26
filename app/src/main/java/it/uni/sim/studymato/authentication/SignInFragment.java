34b10f3ff3e50c0dfb892b25b26165e7630bb17e
Alnu73 <axel8744@gmail.com>
2022-10-26 17:13:33 +0200

    Implemented navigation in authentication

--- app/src/main/java/it/uni/sim/studymato/authentication/SignInFragment.java
+++ app/src/main/java/it/uni/sim/studymato/authentication/SignInFragment.java
@@ -3,10 +3,15 @@ package it.uni.sim.studymato.authentication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import it.uni.sim.studymato.R;
 import it.uni.sim.studymato.databinding.FragmentSignInBinding;
@@ -14,6 +19,8 @@ import it.uni.sim.studymato.databinding.FragmentSignInBinding;
 public class SignInFragment extends Fragment {
 
     FragmentSignInBinding binding = null;
+    private static final String TAG = "SignIn";
+    private FirebaseAuth mAuth;
 
     public SignInFragment() {
         // Required empty public constructor
@@ -23,12 +30,24 @@ public class SignInFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        binding = FragmentSignInBinding.inflate(inflater, container, false);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.auth_nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        binding.tvRedirectLogin.setOnClickListener(v -> {
+            NavDirections action = SignInFragmentDirections.actionSignInFragmentToSignUpFragment();
+            navController.navigate(action);
+        });
+
         return binding.getRoot();
     }
 
--- app/src/main/java/it/uni/sim/studymato/authentication/SignUpFragment.java
+++ app/src/main/java/it/uni/sim/studymato/authentication/SignUpFragment.java
@@ -3,10 +3,15 @@ package it.uni.sim.studymato.authentication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import it.uni.sim.studymato.R;
 import it.uni.sim.studymato.databinding.FragmentSignUpBinding;
@@ -14,6 +19,8 @@ import it.uni.sim.studymato.databinding.FragmentSignUpBinding;
 public class SignUpFragment extends Fragment {
 
     FragmentSignUpBinding binding = null;
+    private static final String TAG = "SignUp";
+    private FirebaseAuth mAuth;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -22,12 +29,24 @@ public class SignUpFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        binding = FragmentSignUpBinding.inflate(getLayoutInflater());
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.auth_nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        binding.tvRedirectRegister.setOnClickListener(v -> {
+            NavDirections action = SignUpFragmentDirections.actionSignUpFragmentToSignInFragment();
+            navController.navigate(action);
+        });
+
         return binding.getRoot();
     }
 
