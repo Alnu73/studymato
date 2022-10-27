d11dbcc60c94c0fd2dec7c73fcb386757ca2f8be
Alnu73 <axel8744@gmail.com>
2022-10-27 09:45:32 +0200

    OnboardingActivity added

---
+++ app/src/main/java/it/uni/sim/studymato/onboarding/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package it.uni.sim.studymato.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.uni.sim.studymato.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
