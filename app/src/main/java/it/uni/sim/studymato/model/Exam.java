8331ecf96308caee85b865315d4369a84ee54eb5
Alnu73 <axel8744@gmail.com>
2022-10-27 15:28:18 +0200

    Model classes updated

--- app/src/main/java/it/uni/sim/studymato/model/Exam.java
+++ app/src/main/java/it/uni/sim/studymato/model/Exam.java
@@ -1,4 +1,40 @@
 package it.uni.sim.studymato.model;
 
+import java.util.Date;
+
 public class Exam {
+
+    private String name;
+    private int credits;
+    private Date dueDate;
+
+    public Exam(String name, int credits, Date dueDate) {
+        this.name = name;
+        this.credits = credits;
+        this.dueDate = dueDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
 }
--- app/src/main/java/it/uni/sim/studymato/model/StudySession.java
+++ app/src/main/java/it/uni/sim/studymato/model/StudySession.java
@@ -1,4 +1,9 @@
 package it.uni.sim.studymato.model;
 
+import java.util.Date;
+
 public class StudySession {
+    private Exam exam;
+    private Date date;
+    private long duration;
 }
