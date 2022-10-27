cba6ba2b01365ce707ccf3cdc3d48d58b17aee06
Alnu73 <axel8744@gmail.com>
2022-10-27 09:51:37 +0200

    OnboardingPageFragment added

---
+++ app/src/main/java/it/uni/sim/studymato/onboarding/OnboardingPageFragment.java
@@ -0,0 +1,30 @@
+package it.uni.sim.studymato.onboarding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.uni.sim.studymato.R;
+
+public class OnboardingPageFragment extends Fragment {
+
+    public OnboardingPageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding_page, container, false);
+    }
+}
