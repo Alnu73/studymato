b4d7d62210dd645c10d3a36905616e2a1af9edfd
Alnu73 <axel8744@gmail.com>
2022-11-07 16:06:06 +0100

    Data retrieval logic implemented

--- app/src/main/java/it/uni/sim/studymato/DashboardFragment.java
+++ app/src/main/java/it/uni/sim/studymato/DashboardFragment.java
@@ -2,13 +2,33 @@ package it.uni.sim.studymato;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.TimeUnit;
+
+import it.uni.sim.studymato.databinding.FragmentDashboardBinding;
+import it.uni.sim.studymato.model.Exam;
+
 public class DashboardFragment extends Fragment {
+
+    FragmentDashboardBinding binding = null;
+    private final FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    private final DatabaseReference ref = mDatabase.getReference();
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -20,9 +40,44 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        DatabaseReference examsRef = ref.child("exams");
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            binding.welcomeTextView.append(currentUser.getDisplayName());
+        }
+        Query q = examsRef.orderByChild("dueDate").limitToFirst(1);
+        q.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    DataSnapshot ds = snapshot.getChildren().iterator().next();
+                    Exam exam = ds.getValue(Exam.class);
+                    if (exam != null) {
+                        Long diff = exam.getDueDate() - System.currentTimeMillis();
+                        Long daysDiff = TimeUnit.MILLISECONDS.toDays(diff);
+                        binding.nextExamTextView.append(daysDiff + " days " + "(" + exam.getName() + ")");
+                    }
+                }
+                else {
+                    binding.nextExamTextView.setText("No exams soon");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
