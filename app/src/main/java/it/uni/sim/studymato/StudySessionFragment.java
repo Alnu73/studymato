1b2559ad3a39988037af5a2a210f5272247d5a92
Alnu73 <axel8744@gmail.com>
2022-11-08 15:59:14 +0100

    Basic features implemented

--- app/src/main/java/it/uni/sim/studymato/StudySessionFragment.java
+++ app/src/main/java/it/uni/sim/studymato/StudySessionFragment.java
@@ -1,23 +1,37 @@
 package it.uni.sim.studymato;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import it.uni.sim.studymato.databinding.FragmentStudySessionBinding;
+import it.uni.sim.studymato.model.StudySession;
 
 public class StudySessionFragment extends Fragment {
 
     FragmentStudySessionBinding binding = null;
+    private CountDownTimer studyTimer;
+    private long studyInterval;
+    private long breakInterval;
+    private StudyIntervals currentInterval;
+    private int numberOfStudyIntervals = 0;
+    private enum StudyIntervals {
+        STUDY,
+        BREAK
+    }
 
     public StudySessionFragment() {
         // Required empty public constructor
@@ -33,6 +47,23 @@ public class StudySessionFragment extends Fragment {
         binding = FragmentStudySessionBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
         toggleBottomNavigationView();
+        studyInterval = 5000;
+        breakInterval = 5000;
+        currentInterval = StudyIntervals.STUDY;
+        binding.breakAndResumeButton.setEnabled(false);
+        setupTimer(studyInterval);
+        studyTimer.start();
+
+        binding.breakAndResumeButton.setOnClickListener(v -> {
+            goToNextInterval();
+            studyTimer.start();
+        });
+
+        binding.endSessionButton.setOnClickListener(v -> {
+            //Dialog
+            studyTimer.cancel();
+        });
+
         return binding.getRoot();
     }
 
@@ -56,6 +87,40 @@ public class StudySessionFragment extends Fragment {
         requireActivity().getOnBackPressedDispatcher().addCallback(
                 this, callback);
     }
+
+    private void setupTimer(long interval) {
+        studyTimer = new CountDownTimer(interval, 1000) {
+            @Override
+            public void onTick(long l) {
+                binding.timerTextView.setText("Time elapsed: " + l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                binding.timerTextView.setText("Done");
+                binding.breakAndResumeButton.setEnabled(true);
+            }
+        };
+    }
+
+    private void goToNextInterval() {
+        binding.breakAndResumeButton.setEnabled(false);
+        //TODO: Send notification
+        if (currentInterval == StudyIntervals.STUDY) {
+            currentInterval = StudyIntervals.BREAK;
+            numberOfStudyIntervals += 1;
+            setupTimer(breakInterval);
+            binding.breakAndResumeButton.setText("Resume");
+            binding.progressTextView.setText(numberOfStudyIntervals*(studyInterval / 1000) + " minutes");
+        }
+        else {
+            currentInterval = StudyIntervals.STUDY;
+            setupTimer(studyInterval);
+            binding.breakAndResumeButton.setText("Take a break");
+        }
+    }
+
+
 
     private void closeWindow() {
         requireActivity().getSupportFragmentManager().popBackStack();
