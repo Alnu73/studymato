5d23c1ba5158a2e60ffffa7c226e9e496ddaa8d8
Alnu73 <axel8744@gmail.com>
2022-11-29 14:53:44 +0100

    Possibility to enable onboarding and change timers added

--- app/src/main/java/it/uni/sim/studymato/SettingsFragment.java
+++ app/src/main/java/it/uni/sim/studymato/SettingsFragment.java
@@ -1,17 +1,17 @@
 package it.uni.sim.studymato;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.annotation.NonNull;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.SwitchPreference;
 import androidx.preference.SwitchPreferenceCompat;
 
-/*
--durata pomodoro
--logout
- */
+import java.util.Objects;
+
+import it.uni.sim.studymato.onboarding.OnboardingPageFragment;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -22,7 +22,20 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        SwitchPreferenceCompat pref = findPreference("onboarding");
+        Objects.requireNonNull(pref).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
+                SharedPreferences.Editor sharedPreferencesEditor =
+                        PreferenceManager.getDefaultSharedPreferences(requireContext()).edit();
+                sharedPreferencesEditor.putBoolean(
+                        OnboardingPageFragment.COMPLETED_ONBOARDING
+                        , pref.isChecked());
+                sharedPreferencesEditor.apply();
+                //TODO: When onboarding is over, set the switch back to unchecked
+                return true;
+            }
+        });
 
     }
 
