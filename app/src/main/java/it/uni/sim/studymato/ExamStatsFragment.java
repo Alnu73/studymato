18dc9ee204307963ff9d87f4af19a13df41ca9fa
Alnu73 <axel8744@gmail.com>
2022-12-22 14:35:40 +0100

    Stats fragment added

---
+++ app/src/main/java/it/uni/sim/studymato/ExamStatsFragment.java
@@ -0,0 +1,40 @@
+package it.uni.sim.studymato;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.uni.sim.studymato.databinding.FragmentDashboardBinding;
+import it.uni.sim.studymato.databinding.FragmentExamStatsBinding;
+
+public class ExamStatsFragment extends Fragment {
+
+    FragmentExamStatsBinding binding = null;
+
+    public ExamStatsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentExamStatsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
