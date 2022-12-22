0fb096e8eec40290d9239cbc844ed3386344b9dd
Alnu73 <axel8744@gmail.com>
2022-12-22 22:20:08 +0100

    Exam stats started

--- app/src/main/java/it/uni/sim/studymato/CustomExamsAdapter.java
+++ app/src/main/java/it/uni/sim/studymato/CustomExamsAdapter.java
@@ -1,19 +1,13 @@
 package it.uni.sim.studymato;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-
-import java.util.ArrayList;
 import java.util.Date;
 
 import it.uni.sim.studymato.model.Exam;
--- app/src/main/java/it/uni/sim/studymato/ExamStatsFragment.java
+++ app/src/main/java/it/uni/sim/studymato/ExamStatsFragment.java
@@ -10,11 +10,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import it.uni.sim.studymato.databinding.FragmentDashboardBinding;
 import it.uni.sim.studymato.databinding.FragmentExamStatsBinding;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ExamStatsFragment extends Fragment {
 
@@ -35,6 +41,25 @@ public class ExamStatsFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentExamStatsBinding.inflate(inflater, container, false);
         toggleBottomNavigationView();
+        Bundle args = this.getArguments();
+        Date dueDate = new Date(args.getLong("examDueDate"));
+        String descriptionText = args.getInt("examCredits") + " credits, for " + String.valueOf(dueDate).substring(0, 10);
+        binding.examNameTextView.setText(args.getString("examName"));
+        binding.examDescriptionTextView.setText(descriptionText);
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<String>();
+        entries.add(new BarEntry(20f, 3));
+        entries.add(new BarEntry(15f, 4));
+        entries.add(new BarEntry(19f, 5));
+        BarChart barChart = binding.barchart;
+
+
+
+        BarDataSet bardataset = new BarDataSet(entries, "Cells");
+        BarData data = new BarData(bardataset);
+        barChart.setData(data); // set the data and list of labels into chart
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        barChart.animateY(2000);
         return binding.getRoot();
     }
 
